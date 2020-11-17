1009b7902fb413d5b07596eab5fd3418ecd3a032
ibct <ibct@ibctui-MacBookPro.local>
2020-11-17 10:57:50 +0900

    init

--- getBlock.js
+++ getBlock.js
@@ -6,6 +6,6 @@ const { JsonRpc } = pkg;
 const rpc = new JsonRpc('http://61.74.164.252:8011', { fetch });
 
 (async () => {
-    const transaction = await rpc.history_get_transaction('27ec2540069bf6771cb3140c985c27d7adb4584d6964c7beed9294834068ddd7', 684627);
+    const transaction = await rpc.get_block(684627);
     console.log(transaction);
   })();
--- getTransaction.js
+++ getTransaction.js
@@ -1,11 +1,12 @@
-
 import pkg from 'eosjs';
 import fetch from 'node-fetch';
 
 const { JsonRpc } = pkg;
-const rpc = new JsonRpc('http://61.74.164.252:8011', { fetch });
+const rpc = new JsonRpc('[IP주소 또는 도메인 이름:포트번호]', { fetch });
+//예시: const rpc = new JsonRpc('http://test.ledgis.io:8011', { fetch });
 
+//트랜잭션 정보 보기 (history_get_transaction)
 (async () => {
-    const transaction = await rpc.get_block(684627);
+    let transaction = await rpc.history_get_transaction('트랜잭션 ID',블록 id); //정보를 보고 싶은 트랜잭션의 아이디(문자열 형식)와 블록번호
     console.log(transaction);
-  })();
+})();
